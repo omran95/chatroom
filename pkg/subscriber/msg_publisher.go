e29c7d6f80b69e544e98395cc4e9bd39c4713ebb
Abdelrahman <abdelrahman.omran95@gmail.com>
2024-06-10 02:47:10 +0300

    remove not needed log

--- pkg/subscriber/msg_publisher.go
+++ pkg/subscriber/msg_publisher.go
@@ -2,14 +2,11 @@ package subscriber
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/omran95/chat-app/pkg/room"
 )
-
-var MessagePubTopic = "chat.msg.pub"
 
 type MessagePublisher interface {
 	PublishToSubscribers(ctx context.Context, subscribers []string, message room.Message) error
@@ -24,7 +21,6 @@ func NewMessagePublisher(publisher message.Publisher) *MessagePublisherImpl {
 }
 
 func (msgPub *MessagePublisherImpl) PublishToSubscribers(ctx context.Context, subscribers []string, msg room.Message) error {
-	fmt.Print(subscribers)
 	for _, subscriber := range subscribers {
 		err := msgPub.publisher.Publish(subscriber, message.NewMessage(watermill.NewULID(), msg.Encode()))
 		if err != nil {
